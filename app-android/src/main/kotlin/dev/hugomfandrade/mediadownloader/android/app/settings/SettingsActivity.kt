17ed0493fdd22c6fc0d11b46ed181fc7e1294a41
hugomfandrade <hugo.mf.andrade@gmail.com>
2025-11-21 17:40:49 +0000

    full compose in settings activity. deletion of xml content

--- app-android/src/main/kotlin/dev/hugomfandrade/mediadownloader/android/app/settings/SettingsActivity.kt
+++ app-android/src/main/kotlin/dev/hugomfandrade/mediadownloader/android/app/settings/SettingsActivity.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.DocumentsContract
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.Modifier
 import dev.hugomfandrade.mediadownloader.android.R
 import dev.hugomfandrade.mediadownloader.android.utils.AndroidMediaUtils
 import dev.hugomfandrade.mediadownloader.android.utils.LegacyAppCompatTheme
@@ -17,7 +20,7 @@ import dev.hugomfandrade.mediadownloader.ui.shared.SettingsScreen
 import dev.hugomfandrade.mediadownloader.ui.shared.Toolbar
 import dev.hugomfandrade.mediadownloader.ui.shared.ToolbarBackButton
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : ComponentActivity() {
 
     companion object {
 
@@ -28,8 +31,6 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_settings)
 
         val directoryUri = AndroidMediaUtils.getDownloadsDirectory(this)
         val directory = directoryUri.cleanPrefix()
@@ -46,40 +47,34 @@ class SettingsActivity : AppCompatActivity() {
                                 Intent.FLAG_GRANT_WRITE_URI_PERMISSION
                     )
 
-                    val docTreeUri = DocumentsContract.getTreeDocumentId(uri)
-                    val path = docTreeUri.substringAfter(":")
+                    val docUri: Uri = DocumentsContract.buildDocumentUriUsingTree(uri, DocumentsContract.getTreeDocumentId(uri))
+                    val path: String? = SettingsUtils.getPath(this, docUri)
 
-                    AndroidMediaUtils.putDownloadsDirectory(this, path)
+                    AndroidMediaUtils.putDownloadsDirectory(this, path?:"")
 
                     summary.value = AndroidMediaUtils.getDownloadsDirectory(this).cleanPrefix()
                 }
             }
 
-        val toolbarComposeView: ComposeView = findViewById(R.id.appbar_compose)
-        toolbarComposeView.setContent {
-            LegacyAppCompatTheme {
-                Toolbar(
-                    title = getString(R.string.settings).uppercase(),
-                    navigationIcon = {
-                        ToolbarBackButton(onClick = { onBackPressedDispatcher.onBackPressed() })
-                    }
-                )
-            }
-        }
-
-        val settingsScreenComposeView: ComposeView = findViewById(R.id.settings_screen)
-        settingsScreenComposeView.setContent {
+        setContent {
 
             val items = listOf(
                 PreferenceData.ClickablePreference(title, summary.value) {
-                    openDocumentTreeLauncher.launch(
-                        AndroidMediaUtils.getDownloadsDirectory(this)
-                    )
+                    openDocumentTreeLauncher.launch(AndroidMediaUtils.getDownloadsDirectory(this))
                 }
             )
 
             LegacyAppCompatTheme {
-                SettingsScreen(items)
+
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Toolbar(
+                        title = getString(R.string.settings).uppercase(),
+                        navigationIcon = {
+                            ToolbarBackButton(onClick = { onBackPressedDispatcher.onBackPressed() })
+                        }
+                    )
+                    SettingsScreen(items)
+                }
             }
         }
     }
